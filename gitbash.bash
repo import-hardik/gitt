c1f70e6285ee42a08ad2763463dab826b118d41a
import-hardik <hardik.57v@gmail.com>
2025-01-04 14:31:04 +0530

    second commit

---
+++ gitbash.bash
@@ -0,0 +1,7 @@
+echo "# gitt" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/import-hardik/gitt.git
+git push -u origin main
